0b47e72b2b4cbbc76d58f8ddf201d31f24c50ed2
Nurcan Yaz <yaznurcan25@gmail.com>
2024-06-30 14:00:00 +0300

    word_embedding_imdb.py eklendi

--- code/word_embedding_imdb.py
+++ code/word_embedding_imdb.py
@@ -29,3 +29,31 @@ cleaned_documents = [clean_text(doc) for doc in documents]
 
 # text tokenization
 tokenized_documents = [simple_preprocess(doc)  for doc in cleaned_documents]
+
+# word2vec modeli tanımla
+model = Word2Vec(sentences=tokenized_documents, vector_size=50, window=5, min_count=1, sg=0)
+word_vectors = model.wv
+
+words = list(word_vectors.index_to_key)[:500]
+vectors = [word_vectors[word] for word in words]
+
+#clustering kmeans k=2
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(vectors)
+clusters = kmeans.labels_
+
+# PCA
+pca = PCA(n_components=2)
+reduced_vectors = pca.fit_transform(vectors)
+
+plt.figure()
+plt.scatter(reduced_vectors[:,0],reduced_vectors[:,1], c= clusters, cmap="viridis")
+
+centers = pca.transform(kmeans.cluster_centers_)
+plt.scatter(centers[:,0], centers[:,1], c ="red", marker = "x", s=150, label = "Center")
+plt.legend()
+
+# figur üzerine kelmelerin eklenmesi
+for i, word in enumerate(words):
+    plt.text(reduced_vectors[i,0], reduced_vectors[i,1],word, fontsize=7)
+plt.title("Word2vec")
